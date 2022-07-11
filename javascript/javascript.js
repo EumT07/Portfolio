459d4e2ab8d115871e85e3cec1710d7b31ed1ab5
EumT07 <eublanmata@gmail.com>
2022-07-11 10:33:39 +0800

    Edited icons - modals - fonts - width

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,11 +2,14 @@
 
 //Dark mode
 let btnDark = document.getElementById("chk");
+let aboutModalInfo = document.querySelector(".about__modal_info")
+let skillsModalInfo = document.querySelector(".skills__modal_info")
 //Funcion dark mode
 btnDark.addEventListener("click", () =>{
     document.body.classList.toggle("dark");
+    aboutModalInfo.style.color = "black";
+    skillsModalInfo.style.color = "black";
 });
-
 
 // btn arrow up
 const btnArrowUp = document.getElementById("btnUp");
@@ -23,22 +26,35 @@ showBtn.addEventListener("mouseleave", ()=>{
 
 
 // About Modal Content
-const aboutMe = document.getElementById("aboutMe");
+const aboutmeBtn = document.getElementById("aboutMe_btn");
 const aboutModalContent = document.getElementById("aboutModalContent");
-const btnAboutModal = document.querySelector(".about__modalBtn");
+const closeBtn_aboutme = document.querySelector(".about_close_btn");
 
 //Open
-aboutMe.addEventListener("click", ()=>{
+aboutmeBtn.addEventListener("click", ()=>{
     aboutModalContent.style.display = "block";
 });
 //Close
-btnAboutModal.addEventListener("click",(e)=>{
+closeBtn_aboutme.addEventListener("click",(e)=>{
     if(e.target.classList.contains("close")){
         aboutModalContent.style.display = "none";
     }
 })
 
+//Skill Modal Content
+const skills_Btn = document.getElementById("skills_Btn");
+const skillModalContent = document.getElementById("skillModalContent");
+const closeBtn_skills = document.querySelector(".skills_close_btn");
 
+skills_Btn.addEventListener("click", ()=>{
+    skillModalContent.style.display = "block";
+})
+
+closeBtn_skills.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("close")){
+        skillModalContent.style.display = "none";
+    }
+})
 
 
 
